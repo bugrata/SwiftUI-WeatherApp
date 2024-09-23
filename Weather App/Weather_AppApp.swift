8d61f91df66340e72864fa0ce16f8a91677c342f
Buğrahan <bugraata2344@gmail.com>
2024-09-23 23:03:04 +0300

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Buğrahan Ata on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
